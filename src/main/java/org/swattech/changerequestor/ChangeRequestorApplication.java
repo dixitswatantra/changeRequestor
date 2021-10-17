0bcac1c619528841b22622ae3371b3b10b5292c7
dixitswatantra <43265292+dixitswatantra@users.noreply.github.com>
2021-10-17 12:26:43 -0400

    main: Initial commit

---
+++ src/main/java/org/swattech/changerequestor/ChangeRequestorApplication.java
@@ -0,0 +1,13 @@
+package org.swattech.changerequestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeRequestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeRequestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/swattech/changerequestor/ChangeRequestorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swattech.changerequestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeRequestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
